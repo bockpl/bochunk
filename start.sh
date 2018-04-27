28e2b3c4b442a80e18631e53a0f1adacf467c481
Seweryn Sitarski <root@ubu1604new>
2018-04-27 15:35:46 +0200

    Poprawka w pliku start.sh

--- start.sh
+++ start.sh
@@ -8,16 +8,4 @@ for DIR in $(grep "/.*" /etc/mfs/mfshdd.cfg); do
 done
 
 # Start the first process
-mfschunkserver start
-RESULT=$?
-if [[ ${RESULT} = "0" ]]; then
-  echo "Failed to start mfschunkserver: $status"
-  exit $status
-fi
-
-while sleep 15; do
-  if $(ps aux |grep mfschunkserver |grep -q -v grep); then
-    echo "Mfschunkserver exited!"
-    exit 1
-  fi
-done
+mfschunkserver -f start
