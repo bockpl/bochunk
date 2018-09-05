e1ade36d2384d0a85e955ad846c389004c5c338b
Seweryn Sitarski <root@ubu1604new>
2018-09-05 12:07:34 +0200

    Poprawa warunku przy ustawianiu wlascicielstwa dla katalogow z chunkami wraz z komentarzem

--- start.sh
+++ start.sh
@@ -6,8 +6,9 @@ for DIR in $(grep "/.*" /etc/mfs/mfshdd.cfg); do
   DIR=$(echo $DIR|sed -e "s@.${MOUNTPOINT}@${MOUNTPOINT}@")
   
   # zmiana wlascicielstwa dla katalogow z chunkami
-  USER_GROUP = "mfs:mfs"
+  USER_GROUP="mfs:mfs"
   if [ $(stat -c %U:%G $DIR) != $USER_GROUP ]; then
+    echo "Change owner for $DIR"
     chown -R $USER_GROUP $DIR
   fi
 done
